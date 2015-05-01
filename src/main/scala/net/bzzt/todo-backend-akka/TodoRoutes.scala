c4a2373d6cd3f254a8f6c064e3d3a502b53d93c4
Arnout Engelen <arnouten@bzzt.net>
2015-05-01 17:25:01 +0200

    Add CORS response headers

--- src/main/scala/net/bzzt/todo-backend-akka/TodoRoutes.scala
+++ src/main/scala/net/bzzt/todo-backend-akka/TodoRoutes.scala
@@ -1,14 +1,22 @@
 package net.bzzt.todo.backend.akka
 
 import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.HttpMethods._
+import akka.http.scaladsl.model.headers._
 
 import akka.http.scaladsl.server._
 import akka.http.scaladsl.server.Directives._
 
 trait TodoRoutes {
   def routes = {
-    get {
-      complete(StatusCodes.OK)
-    }   
+    respondWithHeaders(
+      `Access-Control-Allow-Origin`("*"),
+      `Access-Control-Allow-Headers`("Accept", "Content-Type"),
+      `Access-Control-Allow-Methods`(GET, HEAD, POST, DELETE, OPTIONS, PUT, PATCH)
+    ) {
+      get {
+        complete(StatusCodes.OK)
+      }
+    }
   }
 }
