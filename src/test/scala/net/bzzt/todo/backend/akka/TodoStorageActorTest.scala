b63fca5f0f4f957d921de560cd25e32d87df66aa
Arnout Engelen <arnouten@bzzt.net>
2015-10-25 13:16:14 +0100

    Add test for persistence

---
+++ src/test/scala/net/bzzt/todo/backend/akka/TodoStorageActorTest.scala
@@ -0,0 +1,41 @@
+package net.bzzt.todo.backend.akka
+
+import com.typesafe.config._
+
+import akka._
+import akka.actor._
+import akka.testkit._
+
+import org.scalatest._
+
+class TodoStorageActorTest extends TestKit(
+  ActorSystem("TodoStorageActorTest",
+      ConfigFactory.parseString("""akka.persistence.journal.leveldb.dir=/tmp/journal
+        akka.persistence.snapshot-store.leveldb.dir=/tmp/snapshots""").withFallback(ConfigFactory.load)))
+    with WordSpecLike
+    with BeforeAndAfterAll
+    with ImplicitSender {
+
+  "The Storage-actor" should {
+    "keep state across actor restarts" in {
+      val initialActor = system.actorOf(Props(new TodoStorageActor))
+      initialActor ! TodoStorageActor.Add(TodoUpdate(Some("Todo One"), completed = Some(false), order = Some(1)))
+      expectMsgType[Todo]
+
+      initialActor ! TodoStorageActor.Get
+      val todos = expectMsgType[Iterable[_]]
+
+      system.stop(initialActor)
+
+      val newActor = system.actorOf(Props(new TodoStorageActor))
+      newActor ! TodoStorageActor.Get
+      val newTodos = expectMsgType[Iterable[_]]
+
+      assert(newTodos.size == todos.size)
+    }
+  }
+
+  override def afterAll = {
+    system.shutdown()
+  }
+}
